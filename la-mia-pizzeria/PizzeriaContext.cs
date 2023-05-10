32ea7953a14e25c7c0d202fb5b3d9e440645f65d
Francesco Limpias <francescolimpias@gmail.com>
2023-05-10 15:26:38 +0200

    Created Database connection and run first migrations

---
+++ la-mia-pizzeria/PizzeriaContext.cs
@@ -0,0 +1,17 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static
+{
+    public class PizzeriaContext : DbContext
+    {
+
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=pizzeria;Integrated Security=True;Trust Server Certificate=True");
+        }
+    }
+}
