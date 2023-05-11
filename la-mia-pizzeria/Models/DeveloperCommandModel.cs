fb847907c4a966b7d55d9068c5a23eb10402497e
Francesco Limpias <francescolimpias@gmail.com>
2023-05-11 23:08:51 +0200

    Added Developer system - added DeveloperController - added DeveloperCommandModel - added Developer Index view - added linking button @ Header
    The developer system will provide quick access to tweaking functions directly on runtime! (seeding, basic database management...)
    
    + deleted and gitignored Migrations

---
+++ la-mia-pizzeria/Models/DeveloperCommandModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class DeveloperCommandModel
+    {
+
+        [Key]
+        public long DeveloperCommandModelId { get; set; }
+    }
+}
