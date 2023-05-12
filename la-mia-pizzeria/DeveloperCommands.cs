7c807b8774450a83840360c810b3a460be10a23a
Francesco Limpias <francescolimpias@gmail.com>
2023-05-12 12:08:45 +0200

    Added Category related DeveloperCommands - added seeding logic @ CategorySeeder
    + minor fix @ Developer/Index view
    + added comments @ Developer/Index view

--- la-mia-pizzeria/DeveloperCommands.cs
+++ la-mia-pizzeria/DeveloperCommands.cs
@@ -1,4 +1,6 @@
 ﻿using la_mia_pizzeria_static.Models;
+using la_mia_pizzeria_static.Seeders;
+using Microsoft.EntityFrameworkCore;
 
 namespace la_mia_pizzeria_static
 {
@@ -8,11 +10,30 @@ namespace la_mia_pizzeria_static
         // Developer Commands
         public static List<DeveloperCommandModel> list = new()
         {
+
+            /* CATEGORIES */
+            //clear
+            new()
+            {
+                Name = "Clear Categories",
+                Command = () =>
+                {
+                    using(PizzeriaContext context = new PizzeriaContext())
+                    {
+                        context.Categories.ExecuteDelete();
+                        context.SaveChanges();
+                    }
+                    return true;
+                },
+            },
+
+            //seed      
             new()
             {
                 Name = "Seed Categories",
                 Command = () =>
                 {
+                    CategorySeeder.Run();
                     return true;
                 },
             },
---
+++ la-mia-pizzeria/Seeders/CategorySeeder.cs
@@ -0,0 +1,32 @@
+﻿using la_mia_pizzeria_static.Models;
+
+namespace la_mia_pizzeria_static.Seeders
+{
+    public static class CategorySeeder
+    {
+
+        static List<string> categoriesNames = new List<string>()
+        {
+            "Normale",
+            "Pala",
+            "Maxi",
+            "Metro",
+            "Vegana"
+        };
+
+        public static void Run()
+        {
+
+            using PizzeriaContext context = new PizzeriaContext();
+            {
+                foreach (var categoryName in categoriesNames)
+                {
+                    Category newCategory = new() { Name = categoryName };
+                    context.Categories.Add(newCategory);
+                }
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
