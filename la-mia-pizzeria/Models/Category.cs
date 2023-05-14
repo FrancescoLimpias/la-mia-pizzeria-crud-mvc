c31a4c2ccc228cd8df77ad9e261621b2cf6959f9
Francesco Limpias <francescolimpias@gmail.com>
2023-05-14 15:09:44 +0200

    Added Category's symbol - refactored index view accordingly

--- la-mia-pizzeria/Models/Category.cs
+++ la-mia-pizzeria/Models/Category.cs
@@ -11,6 +11,9 @@ namespace la_mia_pizzeria_static.Models
         [Required]
         public string Name { get; set; }
 
+        [Required]
+        public string Symbol { get; set; }
+
         public List<Pizza> Pizzas { get; set; }
 
         public Category() { }
--- la-mia-pizzeria/Seeders/CategorySeeder.cs
+++ la-mia-pizzeria/Seeders/CategorySeeder.cs
@@ -4,15 +4,14 @@ using System.Diagnostics;
 
 namespace la_mia_pizzeria_static.Seeders
 {
-    public class CategorySeeder : Seeder<string, Category>
+    public class CategorySeeder : Seeder<ValueTuple<string, string>, Category>
     {
         public CategorySeeder() : base(new()
             {
-                "Normale",
-                "Pala",
-                "Maxi",
-                "Metro",
-                "Vegana"
+                ("Classica", "<i class=\"fa-solid fa-pizza-slice\" style=\"color: #d04949;\"></i>"),
+                ("Bianca", "<i class=\"fa-solid fa-pizza-slice\" style=\"color: #ecd3a7;\"></i>"),
+                ("Vegana", "<i class=\"fa-solid fa-seedling\" style=\"color: #1f5137;\"></i>"),
+                ("Marittima", "<i class=\"fa-solid fa-fish\" style=\"color: #2e80b2;\"></i>"),
             })
         { }
 
@@ -21,11 +20,12 @@ namespace la_mia_pizzeria_static.Seeders
             return context.Categories;
         }
 
-        public override Category GenerateElementFromRawData(string name)
+        public override Category GenerateElementFromRawData(ValueTuple<string, string> rawData)
         {
             return new()
             {
-                Name = name,
+                Name = rawData.Item1,
+                Symbol = rawData.Item2,
             };
         }
     }
